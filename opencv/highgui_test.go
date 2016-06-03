7a339d026692052db9a11b814a9c9a8d7bab5177
Ali-Akber Saifee <ali@indydevs.org>
2016-06-04 07:39:10 +0800

    Add tests for highgui load/save image

--- opencv/highgui_test.go
+++ opencv/highgui_test.go
@@ -2,8 +2,31 @@ package opencv
 
 import (
 	"testing"
+	"github.com/lazywei/go-opencv/opencv"
+	"io/ioutil"
 )
 
 func TestLoadImage(t *testing.T) {
-	// t.Errorf("aaa")
+	img := opencv.LoadImage("../images/pic5.png")
+	if img.Width() != 400 || img.Height() != 300 {
+		t.Fatal()
+	}
+}
+
+func TestSaveImage(t *testing.T) {
+	img := opencv.LoadImage("../images/pic5.png")
+	tmpdir, _ := ioutil.TempDir("", "high-gui")
+	filename := tmpdir + "/save_image.jpg"
+	if 1 != opencv.SaveImage(filename, img, 0) {
+		t.Fatal()
+	}
+}
+
+func TestSaveImageInvalidDirectory(t *testing.T) {
+	img := opencv.LoadImage("../images/pic5.png")
+	tmpdir, _ := ioutil.TempDir("", "high-gui")
+	filename := tmpdir + "/invalid/save_image.jpg"
+	if 0 != opencv.SaveImage(filename, img, 0) {
+		t.Fatal()
+	}
 }
